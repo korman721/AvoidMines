efac2d016fb0b94a332672488f3166538da2f03e
korman721 <korman721@gmail.com>
2025-07-15 17:26:44 +0500

    Check

--- Assets/Scripts/Shooter/MoveToPointEffect.cs
+++ Assets/Scripts/Shooter/MoveToPointEffect.cs
@@ -1,19 +1,14 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class MoveToPointEffect : IShootEffect
 {
     private IMovable _movable;
     private PointSetter _pointSetter;
 
-    private NavMeshPath _path;
-
     public MoveToPointEffect(IMovable movable, PointSetter pointSetter)
     {
         _movable = movable;
         _pointSetter = pointSetter;
-
-        _path = new NavMeshPath();
     }
 
     public void Execute(RaycastHit hit)
